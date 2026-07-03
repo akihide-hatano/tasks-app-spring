6cc671748953a03fff653fcb2e8d489e27862fc8
akihide-hatano <a.h.19841202@gmail.com>
2026-07-04 06:06:28 +0900

    testのpackageを作成し、UserControllerTest.javaを追加しました。

---
+++ backend/src/test/java/com/example/taskapiapp/controller/UserControllerTest.java
@@ -0,0 +1,32 @@
+package com.example.taskapiapp.controller;
+
+import com.example.taskapiapp.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private UserService userService;
+
+    @Test
+    void ユーザー一覧取得で200が返却されること() throws Exception {
+        when(userService.findAll()).thenReturn(List.of());
+        mockMvc.perform(get("/users"))
+                .andExpect(status().isOk());
+    }
+}
