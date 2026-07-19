7d7429d4e6640ff5765a1daeb90e56ad81a33b91
akihide-hatano <a.h.19841202@gmail.com>
2026-07-19 13:34:25 +0900

    TaskService.javaのcreateの部分にコメントを追加しました。

--- backend/src/main/java/com/example/taskapiapp/service/TaskService.java
+++ backend/src/main/java/com/example/taskapiapp/service/TaskService.java
@@ -28,12 +28,30 @@ public class TaskService {
     }
 
     public Task create(TaskCreateRequest request){
+        //POSTメソッドで飛んできたrequest内容確認
+        System.out.println("=== Task登録処理 ===");
+        System.out.println("title :" + request.title());
+        System.out.println("description :" + request.description());
+        System.out.println("status :" + request.status());
+        System.out.println("userId :" + request.userId());
 
+        //新しくリクエスト内容を保存してEntityへセットする
+        //Setterを使ってtaskに値をセットする。そのためにEntityはSetterを使用。
         Task task = new Task();
         task.setTitle(request.title());
         task.setDescription(request.description());
         task.setStatus(request.status());
+
+        //Userは存在しているか確認してsetUserを行う。
         task.setUser(userRepository.findById(request.userId()).orElseThrow(() -> new RuntimeException("User not found")));
+
+        //POSTメソッドで飛んできたrequest内容確認
+        System.out.println("=== Task保存前 ===");
+        System.out.println("title :" + task.getTitle());
+        System.out.println("description :" + task.getDescription());
+        System.out.println("status :" + task.getStatus());
+        System.out.println("userId :" + task.getUser().getId());
+
         return taskRepository.save(task);
     }
 
